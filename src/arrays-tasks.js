866c2a805e52ce59ad4d0057f5a11b5fa8e4de18
Dima Usov <dmitry.usov@yahoo.com>
2024-02-07 13:43:08 +0300

    feat: add sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,12 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const longestArrRef = arr1.length > arr2.length ? arr1 : arr2;
+  const smallestArrRef = arr1.length > arr2.length ? arr2 : arr1;
+  return longestArrRef.map((item, idx) => {
+    return item + (idx < smallestArrRef.length ? smallestArrRef[idx] : 0);
+  });
 }
 
 /**
