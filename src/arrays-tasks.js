e95fa951c3b2ce051e596d7f9b7481e5fca48d17
Dima Usov <dmitry.usov@yahoo.com>
2024-02-08 11:27:57 +0300

    feat: add createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -330,8 +330,17 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((newArr, item, idx) => {
+    const chunkIdx = Math.floor(idx / chunkSize);
+
+    if (!newArr[chunkIdx]) {
+      newArr.push([]); // new chunk
+    }
+    newArr[chunkIdx].push(item);
+
+    return newArr;
+  }, []);
 }
 
 /**
